471f651e7ba133215e8c1e4fcccdae4186199117
wanja <awngondi@gmail.com>
2024-05-05 16:40:20 +0300

    python practice

---
+++ python_practice.py
@@ -0,0 +1,36 @@
+#A statement that assigns a value to a variable.
+#To join two operands end to end.
+
+age = 29
+name = "Agnes"
+age = str(29)
+print('My name is '+ name + 'my age is '+ age)
+
+age = 29
+name = "Agnes"
+age = str(29)
+print('My name is '+ name + ' ' + 'my age is '+ age)
+# python programming glossary 
+# Exercise 2
+# Write a program that uses input to prompt a user for their name and then welcomes them.
+name = 'Chuck'
+print('Hello ' + name)
+
+# Exercise 3
+# Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+hours = int(input('no of hours'))
+rate = 2.75
+rate = float(2.75)
+pay = hours * rate
+print(pay)
+
+# Exercise 4
+#Assume that we execute the following assignment statements
+width = 17
+height = float(12.0)
+print(width // 2)
+print(width / 2.0)
+print(height / 3)
+print( 1 + 2 * 5 )
+print(type)
